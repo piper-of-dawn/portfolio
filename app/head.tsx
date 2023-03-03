a4844a2bc4c45182bb56e7d543c0e1067eae596f
piper-of-the-dawn <kumarshan25@gmail.com>
2023-03-03 19:05:10 +0100

    added ogimage url

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Kumar Shantanu</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="title" property="og:title" content="Kumar Shantanu"></meta>
-      <meta name="image" property="og:image" content="/ogphoto.png"></meta>
+      <meta name="image" property="og:image" content="https://github.com/piper-of-dawn/portfolio/blob/2d22adb138ace028c656ee4941178196afbc6a3a/public/ogphoto.png"></meta>
       <meta name="description" property="og:description" content="I'm a design-loving nerd, navigating the intersection of technology, mathematics and art, on this wild ride of figuring out the ways to do something meaningful."></meta>
       
       <link rel="icon" href="/favicon.ico" />
