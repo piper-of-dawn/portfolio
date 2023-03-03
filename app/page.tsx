21f8b3bc88632615b33db678477490574d67a218
piper-of-the-dawn <kumarshan25@gmail.com>
2023-03-03 15:35:51 +0100

    fixed more  font

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ const about = () => {
 
         <div className="about leading-6 p-14 md:pr-8 pt-6 pl-8">
           <div> <div className="font-sans font-light text-center text-verylarge text-slate-700 xl:text-left">Hi I'm </div><p className='font-normal text-verylarge text-center text-slate-700 xl:text-left mt-5 mb-5'>Kumar Shantanu</p></div>
-          <p className='text-normal font-semibold text-slate-900 mb-auto mt-auto pb-4 text-justify'> I'm a design-loving nerd, navigating the intersection of technology, mathematics and art, on this wild ride of figuring out the ways to do something meaningful. </p>
+          <p className='text-normal font-semibold font-mono text-slate-900 mb-auto mt-auto pb-4 text-justify'> I'm a design-loving nerd, navigating the intersection of technology, mathematics and art, on this wild ride of figuring out the ways to do something meaningful. </p>
           <p className='text-normal font-semibold font-mono text-justify text-slate-500 row-start-2'>Econometrics and Time-Series Analysis are my craft and thus I hunt patterns and weave stories with data for a living. I speak Hindi and English to humans, while Python and Javascript to computers.  I am currently learning deep learning, financial engineering and to speak Polish and Rust. In my free time, I develop statistical/econometric models for credit loss forecasting at Union Bank of Switzerland (UBS).</p>
         </div>
      
