497bbd0e9ef4e7b9cb2c6c285a32613080a10d26
piper-of-the-dawn <kumarshan25@gmail.com>
2023-03-03 16:10:22 +0100

    fixed image cdn

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Kumar Shantanu</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="title" property="og:title" content="Kumar Shantanu"></meta>
-      <meta name="image" property="og:image" content="/images/ogphoto.png"></meta>
+      <meta name="image" property="og:image" content="https://github.com/piper-of-dawn/cdn/blob/49fc6dbf0bf2077c2634a56744a938fd2b117b66/ogphoto.png"></meta>
       <meta name="description" property="og:description" content="I'm a design-loving nerd, navigating the intersection of technology, mathematics and art, on this wild ride of figuring out the ways to do something meaningful."></meta>
       
       <link rel="icon" href="/favicon.ico" />
