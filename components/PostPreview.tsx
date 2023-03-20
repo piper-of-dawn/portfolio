9e352c283ab835b80b253850ddc9df60c027551a
piper-of-the-dawn <kumarshan25@gmail.com>
2023-03-20 19:07:06 +0100

    Formatted post previews

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -5,11 +5,13 @@ const PostPreview = (props: PostMetaData ) => {
   return (
 
       <div className="relative group">
-         <div className="absolute inset-4 bg-gradient-to-r rounded-md from-cyan-400 to-cyan-900 blur opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+
+<div className="absolute inset-3 bg-gradient-to-r from-sky-400 to-cyan-900 rounded-lg blur opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+
 
           <div className="relative flex flex-col align-middle p-4 rounded-md bg-white mb-4 h-fit shadow-sm">
            <Link href={`/posts/${props.slug}`}>
-              <h1 className="font-regular text-large overflow-hidden whitespace-nowrap text-sky-900 hover:underline group-hover:overflow-visible">{props.title}</h1>
+              <h1 className="font-bold text-large overflow-hidden whitespace-nowrap text-sky-900 hover:underline group-hover:overflow-visible">{props.title}</h1>
               </Link>
               <h4 className="text-normal text-slate-600">{props.subtitle}</h4>
               <h6 className="font-mono text-subtitle">{props.date}</h6>    
