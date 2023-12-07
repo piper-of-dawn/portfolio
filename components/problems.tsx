56018d9b0044b1dda3f5cb717af49576a23afd43
piper_of_the_dawn <kumarshan25@gmail.com>
2023-12-07 21:19:30 +0100

    Website Update

--- components/problems.tsx
+++ components/problems.tsx
@@ -1,18 +1,20 @@
 const Problems = () => {
-  return (
-<div>
-
-     <div>     
-      <div className="border-l-2 border-slate-800  pl-6 mb-6 mt-6"><p className="text-normal text-slate-800 font-semibold pb-2">A tougher problem I am solving</p>
-      <p className="text-normal text-slate-900 text-justify font-normal">Building an algorithmic trading system through implementing Financial Machine Learning.</p>
-           </div> 
-           <div className="border-l-2 border-slate-800  pl-6 mb-6 mt-6"><p className="text-normal text-slate-800 font-semibold pb-2">The toughest problem I have solved</p>
-      <p className="text-normal text-slate-900 text-justify">Built an attribution tool that mathematically decomposes Expected Credit Loss forecasts into its drivers and presents results as a attribution dashboard, for my current employer UBS.</p>
-           </div> 
-
-           </div>
-</div>
-
-)};
-
-export default Problems;
+     return (
+   <div>
+   
+        <div>     
+         <div className="border-l-2 border-slate-800  pl-6 mb-6 mt-6"><p className="text-normal text-slate-800 font-semibold pb-2">A tougher problem I am solving</p>
+         <p className="text-normal text-slate-900 text-justify font-normal">Building an automated trading system through implementing Financial Machine Learning.</p>
+              </div> 
+              <div className="border-l-2 border-slate-800  pl-6 mb-6 mt-6"><p className="text-normal text-slate-800 font-semibold pb-2">The toughest problem I have solved</p>
+         <p className="text-normal text-slate-900 text-justify">Built a Python library that tests, compares and analyses the difference between two dataframes. Documentation is available at: <a className="text-sky-700 underline" href="www.compare-datasets.kumarshantanu.com" target="_blank" rel="noopener noreferrer">
+           Compare Datasets
+         </a></p>
+              </div> 
+   
+              </div>
+   </div>
+   
+   )};
+   
+   export default Problems;
